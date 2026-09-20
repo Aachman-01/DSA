efcdedca1e05459504ff1f2e28e98ad80fdd77e0
Aachman Dixit <dixitaachman@gmail.com>
2026-09-20 21:56:49 +0530

    Time: 51 ms (82.61%) | Memory: 24 MB (55.32%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -1,58 +1,45 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp=headA;
-        int count1=0;
-        int count2=0;
-        while(temp){
-            count1++;
-            temp=temp->next;
+    /**
+    * Definition for singly-linked list.
+    * struct ListNode {
+    *     int val;
+    *     ListNode *next;
+    *     ListNode(int x) : val(x), next(NULL) {}
+    * };
+    */
+    class Solution {
+    public:
+        ListNode *collisionpoint(ListNode *l1,ListNode *l2,int gap){
+            while(gap){
+                gap--;
+                l2=l2->next;
+            }
+            while (l1 && l2) {
+                if (l1 == l2) return l1;
+                    l1 = l1->next;
+                    l2 = l2->next;
+            }
+            return NULL;
         }
-        temp=headB;
-        while(temp){
-            count2++;
-            temp=temp->next;
-        }
-        int gap=abs(count1-count2);
-        int i=0;
-        ListNode *longer=headA;
-        if(count1>count2){
+
+        ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+            ListNode* temp=headA;
+            int count1=0,count2=0;
+            while(temp){
+                count1++;
+                temp=temp->next;
+            }
+            temp=headB;
+            while(temp){
+                count2++;
+                temp=temp->next;
+            }
+            int gap=abs(count1-count2);
+            ListNode *longer=headA;
             ListNode *shorter=headB;
-            int i=0;
-            while(longer && i<gap){
-                i++;
-                longer=longer->next;
-            }
-            while(shorter){
-                if(shorter == longer){
-                    return longer;
-                }
-                shorter=shorter->next;
-                longer=longer->next;
-            }
-        }else{
-            ListNode *shorter=headB;
-            int i=0;
-            while(shorter && i<gap){
-                i++;
-                shorter=shorter->next;
-            }
-            while(longer){
-                if(shorter == longer){
-                    return longer;
-                }
-                longer=longer->next;
-                shorter=shorter->next;
+            if(count1>count2){
+                return collisionpoint(shorter,longer,gap);
+            }else{
+                return collisionpoint(longer,shorter,gap);
             }
         }
-        return NULL;
-    }
-};
+    };
